73ee4dd7a6916fdcfe7586ba2aea22054cd27409
Alasdair <alasdair123@hotmail.co.uk>
2019-05-02 17:15:20 +0100

    fully working thermostat logic

--- jasmine-standalone-3.4.0/spec/ThermostatSpec.js
+++ jasmine-standalone-3.4.0/spec/ThermostatSpec.js
@@ -11,7 +11,73 @@ describe('Thermostat', function(){
     expect(thermostat.getTemp()).toEqual(20);
   });
 
-  // it('increases by 1' function(){
-  //   expect(thermostat.increase(temp)).to
-  // });
+  it('increases with up()', function(){
+    thermostat.up(1);
+    expect(thermostat.getTemp()).toEqual(21);
+  });
+
+  it('decreases with down()', function(){
+    thermostat.down(10);
+    expect(thermostat.getTemp()).toEqual(10);
+  });
+
+  it('does not go below 10', function(){
+    thermostat.down(11);
+    expect(thermostat.getTemp()).toEqual(10);
+  });
+
+  it('power saving mode is on by default', function(){
+    expect(thermostat.powerSaving).toEqual(true)
+  });
+
+  it('can display powersaving status', function(){
+    expect(thermostat.isPowerSaving()).toEqual(true)
+  });
+
+  it('can toggle PSM', function(){
+    thermostat.togglePowerSaving();
+    expect(thermostat.powerSaving).toEqual(false)
+    thermostat.togglePowerSaving();
+    expect(thermostat.powerSaving).toEqual(true)
+  });
+
+  it('can reset temp to 20', function(){
+    thermostat.up(5);
+    thermostat.reset();
+    expect(thermostat.getTemp()).toEqual(20)
+  });
+
+  describe('when PSM is on', function(){
+    it('has a maximum temp of 25c', function(){
+      //starting at 20c
+      thermostat.up(6);
+      expect(thermostat.getTemp()).toEqual(25)
+    });
+  });
+
+  describe('when PSM is off', function(){
+    it('has a maximum temp of 32c', function(){
+      //starting at 20c
+      thermostat.togglePowerSaving();
+      thermostat.up(13);
+      expect(thermostat.getTemp()).toEqual(32)
+    });
+  });
+
+  describe('showing usage rating', function(){
+    it('returns "medium-usage" at default temp', function(){
+      expect(thermostat.isUsageRating()).toEqual("medium-usage")
+    });
+
+    it('returns "high-usage" when higher than 25', function(){
+      thermostat.up(6);
+      expect(thermostat.isUsageRating()).toEqual("high-usage")
+    });
+
+    it('returns "low usage" when below 18', function(){
+      thermostat.reset();
+      thermostat.down(3);
+      expect(thermostat.isUsageRating()).toEqual("low-usage")
+    });
+  });
 });
--- jasmine-standalone-3.4.0/src/Thermostat.js
+++ jasmine-standalone-3.4.0/src/Thermostat.js
@@ -1,7 +1,55 @@
 function Thermostat() {
   this.temp = 20;
+  this.MIN_TEMP = 10;
+  this.MAX_PS_TEMP = 25;
+  this.MAX_TEMP = 32;
+  this.powerSaving = true;
 }
 
 Thermostat.prototype.getTemp = function(){
   return this.temp
+}
+
+Thermostat.prototype.up = function(number){
+  this.temp += number;
+  if (this.temp > this.isMaximumTemp()) {
+    this.temp = this.isMaximumTemp();
+  }
+}
+
+Thermostat.prototype.down = function(number){
+  this.temp -= number;
+  if (this.temp < this.MIN_TEMP) {
+    this.temp = this.MIN_TEMP;
+  }
+}
+
+Thermostat.prototype.isPowerSaving = function(){
+  return this.powerSaving
+}
+
+Thermostat.prototype.togglePowerSaving = function(){
+  this.powerSaving = !this.powerSaving;
+}
+
+Thermostat.prototype.isMaximumTemp = function(){
+  if (this.powerSaving === true) {
+    return this.MAX_PS_TEMP;
+  } if (this.powerSaving === false) {
+    return this.MAX_TEMP;
+    }
+  }
+
+Thermostat.prototype.reset = function(){
+  this.temp = 20
+}
+
+Thermostat.prototype.isUsageRating = function(){
+  if (this.getTemp() < 18) {
+    return 'low-usage';
+  } else if (this.getTemp() >= 18 && this.getTemp() < 25) {
+    return 'medium-usage';
+  } else  {
+    return 'high-usage';
+  }
 }
